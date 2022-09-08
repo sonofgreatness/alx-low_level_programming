a70a7339b581e6f3ca6b7178136fa6e66a8adeaa
Simphiwe Magagula <simphiwe@ummo.xyz>
2022-09-08 06:13:34 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+/*description*/
+
+int /*description for main*/
+
+main(void)
+
+{
+	printf ("with proper grammar, but the outcome is a piece of art,\n");                                              	return (0);                                                                                       }
