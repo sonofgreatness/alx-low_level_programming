8697d3a5e95eeafacacc04a995fd726a4f380b49
Simphiwe Magagula <simphiwe@ummo.xyz>
2022-09-08 05:07:48 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
-int main(void)
+int main()
 {
-puts("\"Programming is like building a multilingual puzzle"); 
-return (0); 
+puts("\"Prlking is like building a multilingual puzzle");
+return 0;
 }
