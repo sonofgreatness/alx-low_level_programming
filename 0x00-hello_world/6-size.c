25d89caa9f4c88f673a2f5f66aa809c402925b94
Simphiwe Magagula <simphiwe@ummo.xyz>
2022-09-08 10:48:26 -0700

    finale

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -22,15 +22,15 @@ main(void)
 
 
 
-	printf("Size of a char: %zu byte\n", sizeof(charType));
+	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
 
-	printf("Size of an int: %zu bytes\n", sizeof(intType));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
 
-	printf("Size of a long int: %zu bytes\n", sizeof(shortType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(shortType));
 
-	printf("Size of a long long int: %zu bytes\n", sizeof(longType));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(longType));
 
-	printf("Size of a  float: %zu bytes\n", sizeof(floatType));
+	printf("Size of a  float: %zu byte(s)\n", sizeof(floatType));
 
 	return (0);
 
