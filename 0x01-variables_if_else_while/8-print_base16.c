930722150835da9889cb349fc588a0ac3d535fc8
Simphiwe Magagula <simphiwe@ummo.xyz>
2022-09-09 15:46:30 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+/*Program to print ourt all base 16 values */
+
+int /*program entry point*/
+
+main(void)
+
+{
+
+	    int x ;
+
+	        int y ;
+
+		for(x='A';x<='J';x++)/*print 0-9*/
+
+
+
+		{
+
+			    putchar((x%65) + '0' );
+
+
+
+		}
+
+		for(y='a';y<='f';y++)
+
+		{
+
+
+
+			    putchar(y);
+
+		}
+
+		putchar('\n');
+
+		return (0);
+
+}
