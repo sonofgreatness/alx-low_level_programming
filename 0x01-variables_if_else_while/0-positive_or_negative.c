e3e5ba38d8acb85144df8ba06a4eae72a197a97b
Simphiwe Magagula <simphiwe@ummo.xyz>
2022-09-09 11:04:02 -0700

    task 0 retake 2

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -30,14 +30,14 @@ main(void)
 
 	else if(n>0)
        { 
-		printf("%d is positive \n",n);
+		printf("%d is positive\n",n);
 	}
 
 	else
 
 	{  
 
-		printf("%d is negative \n ", n );
+		printf("%d is negative\n", n );
 
 	}
 
