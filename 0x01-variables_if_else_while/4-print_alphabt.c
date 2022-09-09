83cf60c46dbd3f71899c348723e037b500a72003
Simphiwe Magagula <simphiwe@ummo.xyz>
2022-09-09 14:36:34 -0700

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,20 +3,20 @@
 
 /*This program prints letter of the alphabet*/
 
-int/*entry point of of program*/ 
+int/*entry point of of program*/
 
 main()
 
 {
 
-	int x; 
-	for(x = 'a'; x <= 'z'; x++) /*jumps from 'A'(65) to 'B'(66) to ... in order.*/
+	int x;
+	for(x = 'a'; x <= 'z'; x++) /*jumps from 'A'( in order.*/
 
 	{
-	       
-		if (x!='e'&& x!='q')
 
-		
+		if (x !='e' && x != 'q')
+
+
 		{
 			putchar(x); /*the return value is returned as a parameter*/
 
