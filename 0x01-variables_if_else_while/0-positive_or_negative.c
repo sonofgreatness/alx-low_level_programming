5129b3e4eb8aaa3158f240a6f58df101434638a2
Simphiwe Magagula <simphiwe@ummo.xyz>
2022-09-09 01:38:23 -0700

    Task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+
+#include <time.h>
+#include<stdio.h>
+
+
+/* checks if random number is positive or negative*/
+
+int/*entry point for program */
+
+main(void)
+
+{
+
+	int n;
+
+
+
+	srand(time(0));
+
+	n = rand() - RAND_MAX / 2;
+
+	if (n ==0)
+
+	{
+
+		printf("%d is zero\n",n );
+
+	}
+
+	else if(n>0)
+
+
+
+	{
+
+		printf("%d is positive",n);
+
+	}
+
+	else
+
+	{
+
+		printf("%d is negative", n );
+
+	}
+
+		return (0);
+
+}
