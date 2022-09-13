48024fb29f7568fa539c63681ca597e511e476d2
Simphiwe Magagula <simphiwe@ummo.xyz>
2022-09-13 15:43:47 -0700

    9times tble

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+void/*prints times 9 table */
+_table(void){
+
+
+
+	int i;
+
+	int j;
+
+	int ele;
+
+
+
+	for(i = 0; i <= 9; i++)
+
+	{
+
+		for(j = 0 ; j <= 0; j++)
+
+		{
+
+			ele = i*j;
+
+			putchar(ele +'0');
+
+		}
+
+		putchar('\n');
+
+	}
+
+}
