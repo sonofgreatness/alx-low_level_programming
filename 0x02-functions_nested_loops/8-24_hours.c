9b7b41668d6459ab41c7d3aa08089c98718ebfd4
Simphiwe Magagula <simphiwe@ummo.xyz>
2022-09-13 12:47:15 -0700

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+void jack_bauer(void) 
+{
+int d;
+int i; 
+d = 0; 
+while (d <24)
+{
+	i = 0;
+	if(d < 10)
+	{
+		putchar ('0');
+		putchar(d); 
+	}
+	else if (d >= 10)
+	{
+		putchar(i)    
+	}
+	while (i < 60)
+	{
+		if (i < 10)
+		{
+			putchar('0');
+			putchar(i);  
+		}
+		else
+		{
+			putchar(i);   
+		}
+		i++;   
+	}
+	d++;
+}
+}
