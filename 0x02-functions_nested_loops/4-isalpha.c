82fbbdbcce514579d744bdf5ad67191524cfb23a
Simphiwe Magagula <simphiwe@ummo.xyz>
2022-09-13 07:17:54 -0700

    task4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,26 @@
+#include<ctype.h>
+int/*function to check case of a letter*/
+_isalpha(int c)
+{
+
+
+
+	if(isalpha(c))  
+
+	{
+
+		return (1);
+
+	}
+
+	else 
+
+	{
+
+		return (0); 
+
+	}
+
+}
+
+
