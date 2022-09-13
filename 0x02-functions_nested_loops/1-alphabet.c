2ed3bd52d42b23ba7d84dcc2932ec7818b2c99e9
Simphiwe Magagula <simphiwe@ummo.xyz>
2022-09-13 05:10:00 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,24 @@
 #include "main.h"
 
 /*Program */
+/*Print alphabet*/
+void  print_alphabet(void)
+
+{
+
+int i; 
+for (i = 'a'; i <= 'z'; i++)
+{
+
+putchar(i);
+
+}
+
+putchar('\n'); 
+
+}
+
+
 
 int/*main entry point of program */ 
 main(void)
