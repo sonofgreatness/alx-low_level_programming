bcb13618bcffa8caf3964e347d142a7b09c87c53
Simphiwe Magagula <simphiwe@ummo.xyz>
2022-09-13 06:14:43 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,27 @@
+#include<ctype.h>
+
+int
+
+/*function to check case of a letter*/_islower(int c)
+
+{
+
+
+
+	if (islower(c))  
+
+	{
+
+		return (1);
+
+	}
+
+	else 
+
+	{
+
+		return (0); 
+
+	}
+
+}
