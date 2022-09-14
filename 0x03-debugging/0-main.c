cf665b128c6957f05a3d47a697482d7b9cf2da30
Simphiwe Magagula <simphiwe@ummo.xyz>
2022-09-14 04:06:51 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,17 +2,13 @@
 
 
 
-/**
- *
- * * main - tests function that prints if integer is positive or negative
- *
- * * Return: 0
- *
- * */
+/* main - tests function that prints if integer is positive or negative
+ * Return: 0 */
 
 
 
-int main(void)
+int/*entry point for debug */
+main(void)
 
 {
 
