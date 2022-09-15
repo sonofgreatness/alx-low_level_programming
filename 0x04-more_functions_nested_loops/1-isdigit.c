0d09e6cef2382f38d5c2c7fac113347018f05834
Simphiwe Magagula <simphiwe@ummo.xyz>
2022-09-15 00:40:08 -0700

    task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,24 @@
+int /*check if char is digits*/
+_isdigit(int c)
+
+{
+
+	 if (c == 48 || c == 49 ||c == 50 ||c == 51 ||c ==52 || c == 53 || c == 54 || c == 57 || c == 58)
+
+	 {
+
+		 return (1);
+
+	 }
+
+
+
+	 else
+
+	 {
+
+		 return (0);
+
+	 }
+
+}
