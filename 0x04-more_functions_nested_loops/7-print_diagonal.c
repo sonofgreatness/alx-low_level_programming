e204ea6e457b67152052c61e5b1af504f2f0d14e
Simphiwe Magagula <simphiwe@ummo.xyz>
2022-09-15 09:29:53 -0700

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+void/*print  n slashes*/ 
+print_diagonal(int n)
+{
+
+	int i; 
+	int j;
+
+	if (n > 0)
+
+	{
+		for(i = 0; i < n; i++)		
+			{	if (i != 0)
+				{	for(j = 0; j < i;j++)
+					{	
+						putchar(' ');
+					}	
+				
+				}
+				putchar('\\');				
+				putchar('\n');
+			}
+	}
+
+	if (n <= 0)
+
+	{
+
+		putchar('\n');
+
+	}   
+
+}
