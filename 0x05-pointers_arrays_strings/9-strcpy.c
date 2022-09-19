b6577704a7fe178f339db32649b4a1917db36895
Simphiwe Magagula <simphiwe@ummo.xyz>
2022-09-19 15:24:56 -0700

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,10 +1,10 @@
 #include<string.h>
 /*functioton copy*/
-char/*function body*/ 
-*_strcpy(char *dest, char *src)
+char/*function body*/
+* _strcpy(char *dest, char *src)
 {
 
-	strcpy(dest,src);
+	strcpy(dest, src);
 
 	return (dest);
 
