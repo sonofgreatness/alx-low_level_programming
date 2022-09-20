b776cb3301dad38e1a6434735de286d159b09e6a
Simphiwe Magagula <simphiwe@ummo.xyz>
2022-09-19 19:07:23 -0700

    task6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,18 @@
-#include<stdio.h> 
+#include<stdio.h>
 
 #include <string.h>
 
 /*returns lenght of  string*/
 
-int/*get length of string*/ 
+int/*get length of string*/
 
 _strlen(char *s)
 
 {
 
-	const char *cc = (char *) s;                                                                                                                                                                                          
+	const char *cc = (char *) s;
 
-	return (strlen(cc));  
+	return (strlen(cc));
 
 }
 
@@ -20,28 +20,26 @@ _strlen(char *s)
 
 /*prints string*/
 
-void /*function definition*/puts2(char *str)
+void/*function definition*/
+puts2(char *str)
 
 {
 
+int len;
+int i;
+char *t;
 
+len = _strlen(str);
 
-	int len;                                                                                                 
+for (i = 0; i < len; i = i + 2)
 
-	int i;                                                                                                                                                                                                   
+{
 
-	char *t;  	
-	len = _strlen(str);	
-
-	for(i = 0; i < len; i=i+2)
-
-	{                                                                                               
-
-		t = &str[i];                                                             
+		t = &str[i];
 
 		fputc(*t, stdout);
 
-	}
+}
 
 	fputc('\n', stdout);
 
