41ec51409c0ee2f7e181177d4e8ca290bda9e0d2
Simphiwe Magagula <simphiwe@ummo.xyz>
2022-09-19 19:30:28 -0700

    task4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+
+#include <string.h>
+
+/*returns lenght of  string*/
+
+int/*get length of string*/
+
+_strlen(char *s)
+
+{
+
+			const char *cc = (char *) s;
+
+					return (strlen(cc));
+
+}
+
+
+
+/*prints*/
+
+
+
+void/*printa string to stdout */
+
+_puts(char *str)
+
+{
+
+
+
+	fputs(str, stdout);
+
+
+
+}
+/*prints reverse*/
+void/*definition*/ 
+print_rev(char *s)
+
+{
+
+	int i;
+	int j;
+
+	int len;
+	int lena;
+        char *t;
+	len = _strlen(s);
+
+	for (i = 0; i < len; i++)
+	{
+
+	lena = i + 1;
+	j = len - lena;
+	t = &s[j];
+
+
+	fputc(*t, stdout);
+	}
+
+	fputc('\n', stdout);
+
+
+}
