6ed68fdaabffead9982844fe0da1e3efac33b401
Simphiwe Magagula <simphiwe@ummo.xyz>
2022-09-21 03:49:57 -0700

    task1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,69 @@
+#include<string.h>
+/*add 2 strings */
+char
+/*function definition*/
+*_strncat(char *dest, char *src, int n)
+
+{
+
+
+
+	int i;
+
+	int len;
+
+
+	int newlen ;
+
+
+
+	len = strlen(dest);
+
+
+
+
+
+
+	if (n > 0)
+
+	{
+
+		for (i = 0 ; i <= n; i++)
+
+		{
+
+			if (src[i] != '\0')
+
+			{
+
+						dest[len + i] = src[i];
+
+								newlen = len + i;
+
+			}
+
+			else
+
+			{
+
+				i = n;
+
+			}
+
+		}
+
+		dest[newlen + 1] = '\0';
+
+		return (dest);
+
+	}
+
+	else
+
+	{
+
+		return (dest);
+
+	}
+
+}
