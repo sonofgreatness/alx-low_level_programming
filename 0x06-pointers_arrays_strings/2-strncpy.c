f6aa3f96d3baf4bb1f47aa670b64ccc75492e9a9
Simphiwe Magagula <simphiwe@ummo.xyz>
2022-09-22 01:00:45 -0700

    task2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,47 @@
+#include<string.h>
+/*returns lenght of  string*/
+int/*get length of string*/
+_strlen(char *s)
+{
+const char *cc = (char *) s;
+return (strlen(cc));
+}
+
+/*copies string*/
+char/*function definition*/ 
+*_strncpy(char *dest, char *src, int n)
+
+{
+
+
+	int i;
+	char c;
+
+	c = 't';
+
+	for(i =0; i < n ;i++)
+
+	{
+
+		if(src[i] != '\0')
+
+		{
+
+			dest[i] = src[i]; 
+
+		}
+
+		if(c == 's')
+		{
+	  		  dest[i] ='\0';  
+		}
+		
+		if (src[i] == '\0')
+		{ 
+			dest[i] = src[i]; 
+			c = 's';
+		}
+	
+	}
+	return (dest);
+}
