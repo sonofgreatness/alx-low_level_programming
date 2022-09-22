c88c289e2fdecb43098bd16c1662b20a1e59089d
Simphiwe Magagula <simphiwe@ummo.xyz>
2022-09-22 00:58:01 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,20 +1,8 @@
 #include<string.h>
 
-/*returns lenght of  string*/
-int/*get length of string*/
-_strlen(char *s)
-
-{
-const char *cc = (char *) s;
-return (strlen(cc));
-}
-
 /*compares 2 strings */
-
 int/*function definition*/
-
 _strcmp(char *s1, char *s2)
-
 { 
 return (*s1 -   *s2);
 }
