f492b2bd9b6c3efe21a6ee9efdd20e3d73da05ee
Simphiwe Magagula <simphiwe@ummo.xyz>
2022-09-22 19:05:39 -0700

    task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,41 @@
+#include<string.h>
+/*weak encryption*/
+char/*function definition*/
+* rot13(char *x)
+
+{
+
+	int len;
+	int i;
+	int temp;
+	char *p;
+
+	len = strlen(x); 
+
+	for (i = 0; i < len; i++)
+
+	{
+
+		p = &x[i];
+		temp = *p + 13;
+
+		if (temp < 122)
+		{
+			temp = temp - 56;
+
+		}
+
+		else if (temp <= 96 && temp >= 91)
+		{
+
+			temp = temp + 6;
+
+		}
+
+		*(x + i) = temp;
+
+	}
+
+return (x);
+
+}
