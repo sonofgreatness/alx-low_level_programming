63500810c7f42edb8904dbaf9f8e67a5357a40e0
Simphiwe Magagula <simphiwe@ummo.xyz>
2022-09-30 05:55:50 -0700

    task0.2

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /*program to  print it's name*/
 int/*entry point of programs*/
-main(int argc ,char *argv[])
+main(int argc, char *argv[])
 {
 printf("%s \n", argv[argc - argc]);
 return (0);
