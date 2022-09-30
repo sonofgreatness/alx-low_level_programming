73466aeffd4932ce537f99bc556d8450c4b1e4ae
Simphiwe Magagula <simphiwe@ummo.xyz>
2022-09-30 06:55:57 -0700

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,12 @@
+/*program  to print arguments*/
+int /*entry point of program*/
+main(int argc, char *argv[])
+{
+int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s \n", argv[i]);
+	}
+return (0);
+}
