30bdaca412ad22ea53850c1e4c320326ec659c91
Simphiwe Magagula <simphiwe@ummo.xyz>
2022-10-07 07:08:09 -0700

    task0 take2

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,26 +1,14 @@
-#include<stdlib.h>
 /*fn  that allocates memory using malloc. */
 void/*definition of function*/
 * malloc_checked(unsigned int b)
-
 {
-
-
-
-	if (malloc(b) == NULL)
-
-	{
-
-		exit(98);
-
-	}
-
-	else
-
-	{
-
-		return (malloc(b));
-
-	}
+if (malloc(b) == NULL)
+{
+	exit(98);
+}
+else
+{
+	return  (malloc(b));
+}
 
 }
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,1 +1,2 @@
+void _putchar(char c);
 void *malloc_checked(unsigned int b);
