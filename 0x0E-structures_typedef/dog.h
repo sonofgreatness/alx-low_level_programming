008388b845ad4ace67f34f52c07ec1e28cab8c2a
Simphiwe Magagula <simphiwe@ummo.xyz>
2022-10-10 09:27:09 -0700

    task1 take2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,4 @@
+/*right description*/
 #ifndef DOG_H
 #define DOG_H
 /*hold particulars of a dog*/
@@ -5,9 +6,9 @@ struct
 dog
 /*record wit 3 fields*/
 {
-/*struct description*/
 char *name;
 float age;
 char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
