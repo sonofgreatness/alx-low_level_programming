ad9dc33abb107b3248b71309c6fc5033c8d41211
Simphiwe Magagula <simphiwe@ummo.xyz>
2022-10-11 11:54:15 -0700

    task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+/*returns a dog_t variable inituialized*/
+dog_t
+/*function definition*/
+*new_dog(char *name, float age, char *owner)
+{
+
+	char *cname;
+	char *cowner;
+	dog_t dog;
+	dog_t *ptr;
+
+	cname = name;
+	cowner = owner;
+
+
+	dog.name = cname;
+	dog.owner = cowner;
+	og.age = age;
+	ptr = &dog;
+
+
+	return (ptr);
+
+}
