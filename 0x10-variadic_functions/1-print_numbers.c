4cc967e0630f0e9c297dfbfb40261d13019c7f1e
Simphiwe Magagula <simphiwe@ummo.xyz>
2022-10-13 15:09:11 -0700

    task 1'

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdarg.h>
+/*printnumbers*/
+void
+/*functiondefinition*/
+print_numbers(const char *separator, const unsigned int n, ...)
+{
+unsigned int i; 
+int print;
+va_list list; 
+
+va_start(list,n);
+for (i = 0; i < n -1 ; i++)
+{
+print = va_arg(list, int);
+printf("%d%c ", print, *separator);
+va_end(list);
+}
+print = va_arg(list, int); 
+printf("%d" ,print);
+printf("\n");
+}
