61a46d819118a72af47dd0f10beb6748012a64a7
Simphiwe Magagula <simphiwe@ummo.xyz>
2022-10-13 16:14:23 -0700

    task2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdarg.h>
+#include<stdlib.h>
+/*prints strings passed as variables*/
+void
+/*function definition*/
+print_strings(const char *separator, const unsigned int n, ...)
+{
+	char *print;
+	unsigned int i;
+	va_list list;
+
+	va_start(list, n);
+
+	for (i = 0; i < n - 1; i++)
+	{       print = va_arg(list, char *);
+
+		if (print == NULL)
+		{
+			printf("nil%c ", *separator);
+			va_end(list);
+		}
+		else
+		{
+			printf("%s%c ", print, *separator);
+			va_end(list);
+		}
+
+
+	}
+
+	printf("%s", va_arg(list, char *));
+	printf("\n");
+
+}
+
