078b1d24f8819004d7e1d1b2383aeb6230bab62b
Simphiwe Magagula <simphiwe@ummo.xyz>
2022-10-20 14:11:04 -0700

    task2 final take

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /*add node to an exixting list*/
-list_t 
+list_t
 /*fuction definition*/
 *add_node(list_t **head, const char *str)
 {
@@ -12,9 +12,9 @@ new_list = malloc(sizeof(list_t));
 if (new_list == NULL)/*failed to initialize*/
 	return (NULL);
 new_list->str = strdup(str);
-for (size = 0;str[size];size++) /*incrementing variable using a loop*/
-	; 
-new_list->len =size; 
+for (size = 0; str[size]; size++) /*incrementing variable using a loop*/
+	;
+new_list->len = size;
 new_list->next = *head;
 *head = new_list;
 return (*head);
