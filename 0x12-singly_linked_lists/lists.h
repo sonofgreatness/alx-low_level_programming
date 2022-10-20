0cdcf338b844c953dfc5c4225b7f3ac7ac404af8
Simphiwe Magagula <simphiwe@ummo.xyz>
2022-10-20 14:15:50 -0700

    header file addition

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 void print_nil(void);
 int _putchar(char c);
 /*
@@ -21,4 +22,5 @@ struct list_s *next;
 } list_t;
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 #endif/*LIST_H*/
