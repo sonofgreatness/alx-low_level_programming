4619998ca0a0600f34e54218bbd183a6370d67ad
Simphiwe Magagula <simphiwe@ummo.xyz>
2022-10-20 15:47:19 -0700

    task5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ *first function that runs before main 
+ *
+ */
+
+void _attribute_ ((constructor)) first ()
+{
+printf("You're beat and yet you must allow"); 
+printf(",\nI bore my house on mt back!\n");
+}
