3513624d5c4bd57b0cf1430be5f9790332c810cc
Simphiwe Magagula <simphiweiq@gmail.com>
2022-10-24 13:52:41 -0500

    task 1 commit

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdlib.h>
+/*
+ * listint_len -> returns number of elements inlist
+ * @h -> argument (of type : pointer  to a struct)
+*/
+size_t
+/*function defintion*/
+listint_len(const listint_t *h)
+{
+size_t size = 0;
+while (h != NULL)
+{
+
+	if (h->next == NULL)
+	{
+		size++;
+		return (size);
+	}
+	else
+		h = h->next;
+		size++;
+}
+return (size);
+}
