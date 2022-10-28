db8aa142919f90c0bef0993c18136bed3bf4bcaf
Simphiwe Magagula <simphiweiq@gmail.com>
2022-10-27 22:20:57 -0500

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,35 +2,35 @@
 #include <stdio.h>
 /*****CONVERT INT TO BINARY ****/
 /*
-*binary_to_uint - converts binary number to an unsigned int 
+*binary_to_uint - converts binary number to an unsigned int
 * @b - pointer to string of 1s and 0s
-*Return converted number, or 0 there is foreign character in *sring , or if b is a null pointer
+*Return converted number, or 0 there is foreign character in
+*string , or if b is a null pointer
 */
-unsigned int binary_to_uint(const char *b)
-
+unsigned int
+/*function definituion*/
+binary_to_uint(const char *b)
 {
 
-int b_len,base_two; 
+int b_len, base_two;
 unsigned int sum;
 
-sum = 0; 
+sum = 0;
 if (!b)
-	return (0); 
-for (b_len = 0; b[b_len] !='\0'; b_len++)
+	return (0);
+for (b_len = 0; b[b_len] != '\0'; b_len++)
 	;
-	for(b_len--, base_two = 1; b_len >= 0; b_len--, base_two *= 2)
+for (b_len--, base_two = 1; b_len >= 0; b_len--, base_two *= 2)
+{
+	if (b[b_len] != 0 && b[b_len] != 1)
 	{
-		if (b[b_len] != 0 && b[b_len]!= 1)
-		 {      
-			 return (0); 
-		 }	 
-		if (b[b_len] & 1)
-		{
-			sum += base_two; 
-		}
+		return (0);
+	}
+	if (b[b_len] & 1)
+	{
+		sum += base_two;
+	}
 
-	}	
-
-
-return (sum); 
+}
+return (sum);
 }
