9adb49fda4172fbade6c19b1780a76a6469d4a68
Simphiwe Magagula <simphiweiq@gmail.com>
2022-11-01 20:16:37 -0500

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,27 +7,27 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-int fd;
-int wrtr;
-int i;
+	int fd;
+	int wrtr;
+	int i;
 
-if (filename == NULL)
-	return (-1);
+	if (filename == NULL)
+		return (-1);
 
 fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-if (fd == -1)
-	return (-1);
-if (!text_content)
-	 text_content = "";
+	if (fd == -1)
+		return (-1);
+	if (!text_content)
+		text_content = "";
 
-for (i = 0; text_content[i] != '\0'; i++)
+	for (i = 0; text_content[i] != '\0'; i++)
 	;
 
-wrtr = write(fd, text_content, i);
-if (wrtr == -1)
-	return (-1);
+	wrtr = write(fd, text_content, i);
+	if (wrtr == -1)
+		return (-1);
 
-close(fd);
-return (1);
+	close(fd);
+	return (1);
 }
